128ffcfd6bd5c9525e1ff86a9909dceda961acdf
Stalin Huallullo <antonyhuallullo@gmail.com>
2023-03-31 14:47:23 -0500

    add env from socket client

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,13 +1,11 @@
 import React, { createContext } from 'react';
 import { useSocket } from '../hooks/useSocket';
 
-
 export const SocketContext = createContext();
 
 
 export const SocketProvider = ({ children }) => {
-
-    const { socket, online } = useSocket('http://localhost:8080/');
+    const { socket, online } = useSocket(process.env.REACT_APP_URL_SOCKET || "http://localhost:8080/");
 
     return (
         <SocketContext.Provider value={{ socket, online }}>
